ec8d3bbebaa0c6f66cbc29b935f6dd0944e7e434
0xrishi <rishi.mungia@gmail.com>
2023-02-06 23:21:08 +0530

    Project Upstream

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,30 @@
-import '@/styles/globals.css'
+import { useEffect } from "react"
+import AOS from "aos"
+
+import { Inter } from '@next/font/google'
+import 'aos/dist/aos.css'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+import '@/styles/globals.scss'
+
+import Layout from '@/components/Layout/Layout'
+
+const font = Inter({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    AOS.init({
+      easing: "ease-out-cubic",
+      once: true,
+      duration: 500
+    });
+  }, []);
+  
+  return (
+    <Layout>
+      <main className={font.className}>
+        <Component {...pageProps} />
+      </main>
+    </Layout>
+  )
 }
